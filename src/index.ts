83077f2a3eb3c8243d2e780520feb7d26d9989be
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-16 09:56:25 -0500

    set up express

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import configExpress from './config/express';
 
 dotenv.config();
 const port = process.env.PORT || 8080;
 const app = express();
 
 // express config
+configExpress(app);
+// DB config
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
