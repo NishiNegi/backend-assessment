c657cdec4432205e4b8455afc62dbb40995c69fe
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-24 19:59:31 -0500

    create healthcheck endpoint

--- src/config/config.test.ts
+++ src/config/config.test.ts
@@ -1,11 +1,20 @@
-import configExpress from "../config/express"
+import configExpress from "../config/express";
+import configDb from '../config/database';
 
-describe('express', ()=> {
+describe('config', ()=> {
     test('configExpress should return a function', ()=>{
         // Arragne
         const expected = Function
         // Act
         const result = configExpress
+        // Assert
+        expect(result).toBeInstanceOf(expected);
+    })
+    test('configDb should return a function', ()=>{
+        // Arragne
+        const expected = Function
+        // Act
+        const result = configDb
         // Assert
         expect(result).toBeInstanceOf(expected);
     })
