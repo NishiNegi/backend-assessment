0ad381023f7ff2d51554223897ef2ec9897ea62b
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-01-25 07:27:16 -0500

    set password strength verification

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -3,11 +3,26 @@ import crypto from "crypto";
 import { createUser } from "./user.services";
 
 export async function handleCreateUser(req: Request, res: Response) {
-  console.log("in handleCreateUser");
   const data = req.body;
-  const newUser = data;
+
+  function passwordStrengthTest (candidatePassword:string):boolean{
+    const upperCaseLetter = /[A-Z]/;
+    const number = /[0-9]/;
+    return upperCaseLetter.test(candidatePassword) && number.test(candidatePassword);
+  }
   try {
-    //Create user
+    // assert password is in the body of the request
+    if(!data.password)
+    {
+      return res.status(400).json({message: 'password required'});
+    }
+    // assert password is strong enough
+    const passwordStrength = passwordStrengthTest(data.password);
+    if(!passwordStrength)
+    {
+      return res.status(400).json({message: 'Password is not strong enough. Remember to include at least a capital letter and a number.'});
+    }
+    // Create user
     const user = await createUser(data);
 
     return res.status(200).json(user);
